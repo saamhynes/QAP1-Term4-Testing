bae6d09d7509d5baa488933db5c3ab525e80d595
saamhynes <samantha.hynes@keyin.com>
2024-01-30 12:04:26 -0400

    Removed Test

--- src/test/java/com/keyin/SuggestionEngineTest.java
+++ src/test/java/com/keyin/SuggestionEngineTest.java
@@ -87,11 +87,6 @@ public class SuggestionEngineTest {
         assertTrue(suggestions.contains("occurred"));
     }
 
-    @Test
-    public void testFalseWord() {
-        String suggestions = suggestionEngine.generateSuggestions("fnrewuifgbrueyfgyrue");
-        assertFalse(suggestions.isEmpty());
-        assertTrue(suggestions.contains("fnrewuifgbrueyfgyrue"));
-    }
+
 
 }
